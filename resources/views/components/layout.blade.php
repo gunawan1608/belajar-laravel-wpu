f3094bd3c8dd2059a4fd83a8943867485c42744e
Gunawan <tamagunawan08@gmail.com>
2025-08-20 19:15:02 +0700

    Adding Flowbite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
     <title>Halaman Home</title>
 </head>
 
